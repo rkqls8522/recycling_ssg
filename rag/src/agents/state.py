33aaad3d4d7ea3fddeed244a4c6b3e28c0380f4a
jinkyung <genie_lee@hufs.ac.kr>
2026-09-10 11:34:43 +0900

    refactor: agents 파일명, Agentstate 항목 수정

--- rag/src/agents/state.py
+++ rag/src/agents/state.py
@@ -2,12 +2,10 @@ from typing_extensions import List, TypedDict, Dict
 from langchain_core.documents import Document
 
 class AgentState(TypedDict):
-    query: str               # 사용자의 질문
-    context : List[Document] # LLM이 답변에 참고할 문서
-    category : str     # 대분류
-    sub_item : str       # 소분류
-    item_list : List[Dict]   # 분류 이미지별 유사도 리스트
-    region : str             # 지역명
-    img_url : str
-    retry_count : int        # 3 <-> 4 노드 루프 횟수 
-    answer: str 
+    category : str                # 대분류
+    sub_item : str                # 소분류
+    item_list : List[Dict]        # 분류 이미지별 유사도 리스트
+    region : str                  # 지역명
+    img_url : str                 # llm 분류위한 이미지 url
+    is_rag_result : bool          # 4번 노드에서 판단 위한 값 (loop의 여부)
+    final_answer_list : List[str] # 배출 방법
--- rag/src/graph.py
+++ rag/src/graph.py
@@ -1,14 +1,21 @@
 # LangGraph 그래프 조립 (노드 연결)
 
 from langgraph.graph import StateGraph, START, END
-from src.agents.rag_reasoning import AgentState, make_retrieve_node, make_generate_node
+from src.agents.confidence_router import confidence_router
+from src.agents.rule_node import make_rule_node
+from src.agents.classify_node import make_classify_node
+from src.agents.rag_node import AgentState, make_retrieve_node, make_generate_node
 
 
 def build_graph(retriever, llm_config:dict):
     graph_builder = StateGraph(AgentState)
 
-    graph_builder.add_node('retrieve', make_retrieve_node(retriever))
+    graph_builder.add_node(confidence_router())
+    graph_builder.add_node(make_rule_node())
+    graph_builder.add_node(make_classify_node())
+    graph_builder.add_node('retrive', make_retrieve_node(retriever))
     graph_builder.add_node('generate', make_generate_node(llm_config))
+    
 
     graph_builder.add_edge(START, 'retrieve')
     graph_builder.add_edge('retrieve', 'generate')
