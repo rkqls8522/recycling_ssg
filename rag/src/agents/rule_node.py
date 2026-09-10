9536592b1abe80020017adf4031167898400490f
jinkyung <genie_lee@hufs.ac.kr>
2026-09-10 16:58:28 +0900

    refactor: 2. rule_node 개발 기획 반영하여 수정

--- rag/src/agents/rule_node.py
+++ rag/src/agents/rule_node.py
@@ -1,59 +1,9 @@
 from state import AgentState
-from configs.config import load_config
-from prompts.templates import get_rule_prompt
 
-config = load_config()
-k=config["retriever_k"]
-
-# 공통 법령
-def get_baseline(vectorstore, category: str, sub_item:str):
-    filter_dict = {"type":"baseline", "category": category, "sub_item": sub_item}
-    results = vectorstore.similarity_search(
-        query = sub_item, # filter로 이미 좁혀지기 때문에 query는 형식적
-        k=k,
-        filter=filter_dict
-    )
-
-    return results[0] if results else None    
-
-
-# 지자체별 규칙
-def get_exception(vectorstore, category: str, sub_item:str, region:str):
-    filter_dict = {"category": category, "sub_item": sub_item, "region": region}
-    results = vectorstore.similarity_search(
-        query = sub_item, # filter로 이미 좁혀지기 때문에 query는 형식적
-        k=k,
-        filter=filter_dict
-    )
-
-    return results[0] if results else None
-    
-
-def make_rule_node(vectorstore):
+def make_rule_node():
     def rule_node(state: AgentState):
-        category = state['category']
-        sub_item = state['sub_item']
-        region = state['state']
-
-        exception_doc = get_exception(vectorstore, category, sub_item, region)
-        if exception_doc:
-            meta = exception_doc.metadata
-            answer = get_rule_prompt(
-                region = meta['region'],
-                category = meta['category'],
-                sub_item= meta['sub_item'],
-                rule = exception_doc.page_content,
-                exception_type=meta.get("exception_type")
-            )
-        else: 
-            baseline_doc = get_baseline(vectorstore, category, sub_item)
-            answer = get_rule_prompt(
-                region = "공통 기준",
-                category = category,
-                sub_item= sub_item,
-                rule = baseline_doc.page_content if baseline_doc else "관련 규정을 찾을 수 없습니다."
-            )
-
-        return {"answer":answer}
+        return {"category",state['category'], 
+                "sub_item",state['sub_item'],
+                "is_rag_result", False}
     
     return rule_node
--- rag/src/prompts/templates.py
+++ rag/src/prompts/templates.py
@@ -1,20 +1,6 @@
 from agents.state import VerificationSubState
 
 
-# 2. RULE NODE
-def get_rule_prompt(region:str, category:str, sub_item:str, rule:str, exception_type:str=None):
-    header = f"[{category} - {sub_item}] 분리배출 안내"
-    region_line = f"📍 지역: {region}"
-    rule_line = f"♻️ 배출 방법: {rule}"
-
-    lines = [header, region_line, rule_line]
-
-    if exception_type:
-        lines.append(f"⚠️ 이 지역만의 예외 규정입니다 ({exception_type})")
-
-    return "\n".join(lines)
-
-
 # 3. LLM NODE
 def get_llm_prompt():
     return (f"""
